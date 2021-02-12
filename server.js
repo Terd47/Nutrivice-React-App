e5730a7be75b01fa77f5500dad413eb35b2a9955
Terd47 <terdlar@gmail.com>
2021-02-12 10:20:21 -0500

    updated middleware

--- server.js
+++ server.js
@@ -17,12 +17,9 @@ app.use(
 );
 app.use(bodyParser.json());
 
-if(process.env.NODE_ENV !== "production"){
-  app.options("*", cors());
-  app.use(cors());
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
-} 
-
+}
 
 const PORT = process.env.PORT || 5000;
 // connect to database
@@ -37,6 +34,12 @@ mongoose.connect(
 )
 .then(() => console.log("Successful connection to mongoDB"))
 .catch( err => console.log(err));
+
+
+if(process.env.NODE_ENV !== "production"){
+  app.options("*", cors());
+  app.use(cors());
+} 
 
 app.use(express.json());
 
